541519f56901c400d658e80309ebdf172d1138dd
Shahzad Manzoor <shahzad@brokergenius.com>
2016-08-03 14:39:32 +0500

    app config update

--- config/app.php
+++ config/app.php
@@ -155,7 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        //Intervention\Image\ImageServiceProvider::class,
         App\Providers\RocketShipServiceProvider::class,
         App\Providers\GuzzleServiceProvider::class,
 
